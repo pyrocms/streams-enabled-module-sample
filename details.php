86d1f989c4ee491e63da375ae378a93848dba97e
Adam Fairholm <adam.fairholm@gmail.com>
2012-03-16 14:07:10 -0400

    Where did this come from.

--- details.php
+++ details.php
@@ -1,4 +1,4 @@
-deta<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 class Module_Faq extends Module {
 
