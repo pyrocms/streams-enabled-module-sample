47225b81df83539ce2a2a903dc06c6f86009e0d0
Samuel Hilson <shilson@coolandofficial.com>
2012-06-06 11:36:16 -0600

    frontend controller now sends faq array to view.

--- controllers/faq.php
+++ controllers/faq.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Faq extends Public_Controller 
+class Faq extends Public_Controller
 {
 
     /**
@@ -11,14 +11,28 @@ class Faq extends Public_Controller
      * @access public
      * @return void
      */
-    public function __construct() 
-	{
+    public function __construct()
+    {
         parent::__construct();
+        $this->load->driver('Streams');
+    }
 
-	}
-	public function index()
-	{
+    public function index()
+    {
+        $params = array(
+            'stream' => 'faqs',
+            'namespace' => 'faq',
+            'paginate' => 'yes',
+            'page_segment' => 4
+        );
 
-	}
+        $this->data->faqs = $this->streams->entries->get_entries($params);
+
+        // Build the page
+        $this->template->title($this->module_details['name'])
+                ->build('index', $this->data);
+    }
+
 }
+
 /* End of file faq.php */
