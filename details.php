a5706738da0273875d07f32a15d0937a4ef7d5f5
Samuel Hilson <shilson@coolandofficial.com>
2012-06-06 11:35:23 -0600

    Formatting cleanup

--- details.php
+++ details.php
@@ -1,95 +1,96 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
 
-class Module_Faq extends Module {
+defined('BASEPATH') or exit('No direct script access allowed');
 
-	public $version = '1.0';
+class Module_Faq extends Module
+{
 
-	public function info()
-	{
-		return array(
-			'name' => array(
-				'en' => 'FAQ'
-			),
-			'description' => array(
-				'en' => 'Frequently Asked Questions'
-			),
-			'frontend' => TRUE,
-			'backend' => TRUE,
-			'menu' => 'content',
-			'shortcuts' => array(
-				'create' => array(
-					'name' 	=> 'faq:new',
-					'uri' 	=> 'admin/faq/create',
-					'class' => 'add'
-					)
-				)
-		);
-	}
+    public $version = '1.0';
 
-	public function install()
-	{
-		// We're using the streams API to
-		// do data setup.
-		$this->load->driver('Streams');
-		
-		$this->load->language('faq/faq');
-	
-		// Add dogs streams
-		if ( ! $this->streams->streams->add_stream(
-										lang('faq:faqs'),
-										'faqs',
-										'faq',
-										'faq_',
-										NULL
-									)) return FALSE;
-									
-		// Add some fields
-		$fields = array(
-			array(
-				'name'			=> 'Question',
-				'slug'			=> 'question',
-				'namespace'		=> 'faq',
-				'type'			=> 'text',
-				'extra'			=> array('max_length' => 200),
-				'assign'		=> 'faqs',
-				'title_column'	=> TRUE,
-				'required'		=> TRUE,
-				'unique'		=> TRUE
-			),
-			array(
-				'name'			=> 'Answer',
-				'slug'			=> 'answer',
-				'namespace'		=> 'faq',
-				'type'			=> 'textarea',
-				'assign'		=> 'faqs',
-				'required'		=> TRUE
-			)
-		);
-		
-		$this->streams->fields->add_fields($fields);
-		
-		return TRUE;
-	}
+    public function info()
+    {
+        return array(
+            'name' => array(
+                'en' => 'FAQ'
+            ),
+            'description' => array(
+                'en' => 'Frequently Asked Questions'
+            ),
+            'frontend' => TRUE,
+            'backend' => TRUE,
+            'menu' => 'content',
+            'shortcuts' => array(
+                'create' => array(
+                    'name' => 'faq:new',
+                    'uri' => 'admin/faq/create',
+                    'class' => 'add'
+                )
+            )
+        );
+    }
 
-	public function uninstall()
-	{
-		$this->load->driver('Streams');
-		
-		$this->streams->utilities->remove_namespace('faq');
-		
-		return TRUE;
-	}
+    public function install()
+    {
+        // We're using the streams API to
+        // do data setup.
+        $this->load->driver('Streams');
 
-	public function upgrade($old_version)
-	{
-		// Your Upgrade Logic
-		return TRUE;
-	}
+        $this->load->language('faq/faq');
 
-	public function help()
-	{
-		// Return a string containing help info
-		// You could include a file and return it here.
-		return "No documentation has been added for this module.<br />Contact the module developer for assistance.";
-	}
+        // Add faqs streams
+        if (!$this->streams->streams->add_stream(
+                        lang('faq:faqs'), 'faqs', 'faq', 'faq_', NULL
+        ))
+            return FALSE;
+
+        // Add some fields
+        $fields = array(
+            array(
+                'name' => 'Question',
+                'slug' => 'question',
+                'namespace' => 'faq',
+                'type' => 'text',
+                'extra' => array('max_length' => 200),
+                'assign' => 'faqs',
+                'title_column' => TRUE,
+                'required' => TRUE,
+                'unique' => TRUE
+            ),
+            array(
+                'name' => 'Answer',
+                'slug' => 'answer',
+                'namespace' => 'faq',
+                'type' => 'textarea',
+                'assign' => 'faqs',
+                'required' => TRUE
+            )
+        );
+
+        $this->streams->fields->add_fields($fields);
+
+        return TRUE;
+    }
+
+    public function uninstall()
+    {
+        $this->load->driver('Streams');
+
+        $this->streams->utilities->remove_namespace('faq');
+
+        return TRUE;
+    }
+
+    public function upgrade($old_version)
+    {
+        // Your Upgrade Logic
+        return TRUE;
+    }
+
+    public function help()
+    {
+        // Return a string containing help info
+        // You could include a file and return it here.
+        return "No documentation has been added for this module.<br />Contact the module developer for assistance.";
+    }
+
 }
