ea7f4eb2f68b52cbb432da152579d075334077b0
Samuel Hilson <shilson@coolandofficial.com>
2012-06-06 11:34:15 -0600

    Formatting Cleanup.

--- language/english/faq_lang.php
+++ language/english/faq_lang.php
@@ -3,9 +3,9 @@
 $lang['faq:new']				= 'New FAQ';
 $lang['faq:faq']				= 'FAQ';
 $lang['faq:faqs']				= 'FAQs';
-$lang['faq:no_faqs']			= 'There are currently no FAQs';
-$lang['faq:question']			= 'Question';
+$lang['faq:no_faqs']                            = 'There are currently no FAQs';
+$lang['faq:question']                           = 'Question';
 $lang['faq:answer']				= 'Answer';
 
-$lang['faq:submit_success']		= 'New FAQ submitted successfully.';
-$lang['faq:submit_failure']		= 'There was a problem submitting your FAQ.';
+$lang['faq:submit_success']                     = 'New FAQ submitted successfully.';
+$lang['faq:submit_failure']                     = 'There was a problem submitting your FAQ.';
