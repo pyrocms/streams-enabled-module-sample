90bcd8d38efd8e35f8436f504dea014b4a28fa3b
Samuel Hilson <shilson@coolandofficial.com>
2012-06-06 11:09:53 -0600

    Added start of frontend for Streams faq sample.

---
+++ controllers/faq.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Faq extends Public_Controller 
+{
+
+    /**
+     * The constructor
+     * @access public
+     * @return void
+     */
+    public function __construct() 
+	{
+        parent::__construct();
+
+	}
+	public function index()
+	{
+
+	}
+}
+/* End of file faq.php */
